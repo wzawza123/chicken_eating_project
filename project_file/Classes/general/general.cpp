e44656b4f898302d14919c7bce06954bd2e42926
wza <936956317@qq.com>
2021-06-02 20:14:42 +0800

    player ui and map integration

--- project_file/Classes/general/general.cpp
+++
@@ -1,7 +0,0 @@
-#include"general.h"
-void unitizeVec(Point* pot) 
-{
-    float length = sqrt(pot->x * pot->x + pot->y * pot->y);
-    pot->x /= length;
-    pot->y /= length;
-}
--- project_file/Classes/general/general.h
+++
@@ -1,6 +0,0 @@
-#ifndef GENERAL_H
-#define GENERAL_H
-#include<cocos2d.h>
-using namespace cocos2d;
-void unitizeVec(Point* pot);
-#endif
